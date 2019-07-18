3201340f07a4728d728a3086b160492d113e110a
Fatming <osimejp@yahoo.co.jp>
2019-07-18 09:25:07 +0800

    add dummy node type

--- MessageList.h
+++ MessageList.h
@@ -32,23 +32,23 @@ enum eNodeType
 	eNT_CUSTOMER,
 	eNT_CHARGE_POINT,
 	eNT_DELIVER_POINT,//it also is a charge point,need this one?.server need to detect the deliver point is empty and ask idle card to move to next charge point or to deliver point!?
-	eNT_TURN_POUNT_R15 = 0x10,//15
+	eNT_TURN_POUNT_R15,//15
 	eNT_TURN_POUNT_R30,//30
 	eNT_TURN_POUNT_R45,//45
 	eNT_TURN_POUNT_R60,//60
 	eNT_TURN_POUNT_R75,//75
 	eNT_TURN_POUNT_R90,//90
-	eNT_TURN_POUNT_R105,//105
+	eNT_TURN_POUNT_R105,//105,10
 	eNT_TURN_POUNT_R120,//120
 	eNT_TURN_POUNT_R135,//135
 	eNT_TURN_POUNT_R150,//150
 	eNT_TURN_POUNT_R165,//165
 	eNT_TURN_POUNT_R180,//180
-	eNT_TURN_POUNT_L15 = 0x8010,//15
+	eNT_TURN_POUNT_L15,//15
 	eNT_TURN_POUNT_L30,//30
 	eNT_TURN_POUNT_L45,//45
 	eNT_TURN_POUNT_L60,//60
-	eNT_TURN_POUNT_L75,//75
+	eNT_TURN_POUNT_L75,//75,20
 	eNT_TURN_POUNT_L90,//90
 	eNT_TURN_POUNT_L105,//105
 	eNT_TURN_POUNT_L120,//120
@@ -56,6 +56,7 @@ enum eNodeType
 	eNT_TURN_POUNT_L150,//150
 	eNT_TURN_POUNT_L165,//165
 	eNT_TURN_POUNT_L180,//180
+	eNT_DUMMY_POINT_FOR_COLLISION_TEST,//a not exists node (no rfid card),only for route collision test.
 	eNT_MAX,
 };
 
