526b5b00b904f789cce6bd59da83f3529a0b6798
Fatming <osimejp@yahoo.co.jp>
2019-07-29 17:07:08 +0800

    add result string

--- CarGoAppSharedProtocolResult.h
+++ CarGoAppSharedProtocolResult.h
@@ -24,6 +24,31 @@ enum eCarGoAppSharedProtocolResult
 	eCGASPR_OK = 0,
 	eCGASPR_VERSION_NOT_MATCH,
 	eCGASPR_VERSION_MAP_NAME_NOT_MATCH,
-	eCGASPR_DELIVERPOINT_OCCUPIED,
+	eCGASPR_DELIVERPOINT_OCCUPIED_BY_OTHER_CAR_GO_APP,
+	eCGASPR_DELIVERPOINT_NO_CAR,
+	eCGASPR_SERVER_ERROR_PLEASE_RELOGIN_AGAIN,//
 	eCGASPR_MAX
 };
+
+inline const wchar_t* ResultToString(eCarGoAppSharedProtocolResult e_eCarGoAppSharedProtocolResult)
+{
+	switch (e_eCarGoAppSharedProtocolResult)
+	{
+		case eCGASPR_OK:
+			return L"Ok";
+		case eCGASPR_VERSION_NOT_MATCH:
+			return L"version is not matched";
+		case eCGASPR_VERSION_MAP_NAME_NOT_MATCH:
+			return L"map file name is not matched";
+		case eCGASPR_DELIVERPOINT_OCCUPIED_BY_OTHER_CAR_GO_APP:
+			return L"deliver point is occupied by other car go app";
+		case eCGASPR_DELIVERPOINT_NO_CAR:
+			return L"no cat at this deliver point";
+		case eCGASPR_SERVER_ERROR_PLEASE_RELOGIN_AGAIN:
+			return L"server shut down please restart app.";
+		default:
+			return L"not support result";
+			break;
+	}
+	return L"not support result";
+}
