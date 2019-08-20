2c0600fd50659f95446e8478a7a6eeb89d2acf00
Fatming <osimejp@yahoo.co.jp>
2019-08-20 14:08:51 +0800

    add streaming data protocol

---
+++ RasPI_Arduino_UART_Portocol.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "StreamingDataReceiver.h"
+
+enum eArduinoRasPIMessage
+{
+	eARM_A2R_HWARDWARE_INFO = 0,
+	eARM_R2A_COMMAND,
+	eARM_A2R_MAX
+};
+
+//make sure
+#pragma pack(push)  /* push current alignment to stack */
+#pragma pack(1)     /* set alignment to n byte boundary */
+LAZY_HEADER_STAR(eARM_A2R_HWARDWARE_INFO)
+	uint16	ui16Obstacle;
+	uint16	ui16TagID;
+	uint16	ui16LeftLoad;
+	uint16	ui16RightLoad;
+	uint16	ui16LeftSpeed;
+	uint16	ui16RightSpeed;
+	uint16	ui16Power;
+LAZY_HEADER_END(eARM_A2R_HWARDWARE_INFO)
+#pragma pack()     /* cancel previous alignment to n byte boundary */ 
+
+
+#pragma pack(push)  /* push current alignment to stack */
+#pragma pack(1)     /* set alignment to n byte boundary */
+LAZY_HEADER_STAR(eARM_R2A_COMMAND)
+LAZY_HEADER_END(eARM_R2A_COMMAND)
+#pragma pack()     /* cancel previous alignment to n byte boundary */ 
+
+
+
+
+
+//int	SendDataTestCode(char*e_pBuffer, int e_iSizeToReceived)
+//{
+//	sStreamingData_eARM_A2R_HWARDWARE_INFO l_Data;
+//	l_Data.i16Message;
+//	l_Data.i16PacketSize;
+//	//l_Data.ui8MagicID;
+//	l_Data.ui16LeftSpeed = 1;
+//	l_Data.ui16LeftLoad = 2;
+//	l_Data.ui16Obstacle = 3;
+//	l_Data.ui16Power = 4;
+//	l_Data.ui16RightSpeed = 5;
+//	l_Data.ui16RightLoad = 6;
+//	l_Data.ui16TagID = 7;
+//
+//	l_Data.i16CheckSum = BufferAndSizeGetCheckSum((char*)&l_Data, sizeof(sStreamingData_eARM_A2R_HWARDWARE_INFO));
+//	char l_Garbage[3] = { 55,68,63 };
+//	char l_Garbage2[3] = { 99,1,0 };
+//	memcpy(e_pBuffer, l_Garbage, sizeof(l_Garbage));
+//	memcpy(&e_pBuffer[3], &l_Data, sizeof(l_Data));
+//	memcpy(&e_pBuffer[sizeof(l_Data) + sizeof(l_Garbage)], l_Garbage2, sizeof(l_Garbage2));
+//	return sizeof(l_Data) + sizeof(l_Garbage) + sizeof(l_Garbage2);
+//}
+//
+//inline void  ReceiveTestCode()
+//{
+//	sBufferAndSize l_sBufferAndSize;
+//	BufferAndSizeInit(&l_sBufferAndSize);
+//	char*l_pData = 0;
+//	int	l_iOutSize = 0;
+//	while (1)
+//	{
+//		char pBuffer[999];
+//		//uart
+//		int l_iSize = rec(pBuffer, 999);
+//		if (l_iSize)
+//		{
+//			if (BufferAndSizeAddBuffer(&l_sBufferAndSize, pBuffer, l_iSize) != -1)
+//			{
+//				//do your things
+//				int16 l_i16MessageID = -1;
+//				l_pData = BufferAndSizeGetData(&l_sBufferAndSize, &l_iOutSize, &l_i16MessageID);
+//				if (l_pData)
+//				{
+//					//do your things
+//					if (l_i16MessageID == eARM_A2R_HWARDWARE_INFO)
+//					{
+//						sStreamingData_eARM_A2R_HWARDWARE_INFO*l_pFinalData = (sStreamingData_eARM_A2R_HWARDWARE_INFO*)l_pData;
+//						l_pFinalData->ui16LeftLoad = 0;
+//					}
+//					//
+//					free(l_pData);
+//				}
+//			}
+//		}
+//	}
+//	BufferAndSizeFree(&l_sBufferAndSize);
+//}
---
+++ StreamingDataReceiver.h
@@ -0,0 +1,176 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef int8
+#define int8 char
+#endif
+
+#ifndef int16
+#define int16 short
+#endif
+
+#ifndef uint8
+#define uint8 unsigned char
+#endif
+#ifndef uint16
+#define uint16 unsigned short
+#endif
+
+
+#define		LAZY_HEADER_STAR(ID)			struct sStreamingData_##ID{int8 i8MagicID[MAGIC_ID_COUNT]; int16	i16PacketSize; uint16	i16CheckSum; int16 i16Message;
+#define		LAZY_HEADER_END(ID)				sStreamingData_##ID(){ memset(this,0,sizeof(sStreamingData_##ID));ASSIGN_MAGIC_ID(i8MagicID);i16PacketSize = sizeof(sStreamingData_##ID); i16CheckSum = 0;i16Message = ID;} };
+
+
+#define		MAGIC_ID_COUNT					4
+#define		MAGIC_ID_SIZE					(sizeof(int8)*MAGIC_ID_COUNT)
+#define		PACKET_SIZE_SIZE				sizeof(int16)
+#define		CHECK_SUM_SIZE					sizeof(uint16)
+#define		MESSAGE_ID_SIZE					sizeof(int16)
+#define		HEADER_SIZE						(MAGIC_ID_SIZE+PACKET_SIZE_SIZE+CHECK_SUM_SIZE+MESSAGE_ID_SIZE)
+
+#define		MAGIC_ID_INDEX					0
+#define		PACKET_SIZE_INDEX				MAGIC_ID_SIZE
+#define		CHECK_SUM_INDEX					(PACKET_SIZE_INDEX+PACKET_SIZE_SIZE)
+#define		MESSAGE_ID_INDEX				(CHECK_SUM_INDEX+sizeof(uint16))
+
+
+
+const static int8	g_i8StreamingDataMagicID[MAGIC_ID_COUNT] = { 55,68,63,80 };
+#define		ASSIGN_MAGIC_ID(Data)			memcpy(Data, g_i8StreamingDataMagicID,MAGIC_ID_SIZE);
+
+//MyStructure2 ll;
+//ll.a = 9;
+//ll.b = 10;
+//ll.i16CheckSum = GetCheckSum(&ll, size);
+//send(ll, ll.iSize);
+//
+
+
+inline int BufferAndSizeGetCheckSum(char*e_pData,int e_iSize)
+{
+	int l_iCheckSum = 0;
+	for (int i = 0; i < e_iSize; ++i)
+	{
+		l_iCheckSum += e_pData[i];
+	}
+	return l_iCheckSum;
+}
+#define	NUM_MAGIC_ID_MATCHED	10
+typedef struct sBufferAndSize sBufferAndSize;
+struct sBufferAndSize
+{
+	char*	pBuffer;
+	int		iSize;
+};
+
+inline void	BufferAndSizeInit(sBufferAndSize*e_pStreamingDataReceiver){e_pStreamingDataReceiver->pBuffer = 0; e_pStreamingDataReceiver->iSize = 0;}
+inline void	BufferAndSizeFree(sBufferAndSize*e_pStreamingDataReceiver) { if (e_pStreamingDataReceiver->pBuffer)free(e_pStreamingDataReceiver->pBuffer); e_pStreamingDataReceiver->iSize = 0; }
+inline void	BufferAndSizeRemoveBuffer(sBufferAndSize*e_pStreamingDataReceiver,int e_iSize)
+{
+	char*l_pNewBuffer = 0;
+	int l_iNewSize = e_pStreamingDataReceiver->iSize-e_iSize;
+	if (l_iNewSize == 0)
+	{
+		BufferAndSizeFree(e_pStreamingDataReceiver);
+		return;
+	}
+	l_pNewBuffer = (char*)malloc(l_iNewSize);
+	memcpy(l_pNewBuffer, &e_pStreamingDataReceiver[e_iSize], l_iNewSize);
+	BufferAndSizeFree(e_pStreamingDataReceiver);
+	e_pStreamingDataReceiver->pBuffer = l_pNewBuffer;
+	e_pStreamingDataReceiver->iSize = l_iNewSize;
+}
+inline int		BufferAndSizeAddBuffer(sBufferAndSize*e_pStreamingDataReceiver, char*e_pNewBuffer, int e_iSize)
+{
+	int l_iNewSize = e_iSize + e_pStreamingDataReceiver->iSize;
+	char*l_pNewBuffer = 0;
+	//old data over 4k???remove old data
+	if (l_iNewSize > 4096)
+	{
+		printf("buffer over 4096 bytes");
+		BufferAndSizeFree(e_pStreamingDataReceiver);
+		return -1;
+	}
+	if (e_iSize == 0)
+		return -1;
+	//malloc old and new buffer size
+	l_pNewBuffer = (char*)malloc(l_iNewSize);
+	//copy old
+	if (e_pStreamingDataReceiver->iSize)
+		memcpy(l_pNewBuffer, e_pStreamingDataReceiver->pBuffer, e_pStreamingDataReceiver->iSize);
+	//copy new
+	memcpy(&l_pNewBuffer[e_pStreamingDataReceiver->iSize], e_pNewBuffer, e_iSize);
+	//free old
+	if (e_pStreamingDataReceiver->pBuffer)
+		free(e_pStreamingDataReceiver->pBuffer);
+	//assign to new
+	e_pStreamingDataReceiver->pBuffer = l_pNewBuffer;
+	e_pStreamingDataReceiver->iSize += e_iSize;
+	return 1;
+}
+inline int		BufferAndSizeGetMatchedMagicID(sBufferAndSize*e_pStreamingDataReceiver, int*e_piMatchedIndex, int*e_piNumMatched)
+{
+	int		l_iBufferSize = e_pStreamingDataReceiver->iSize - MAGIC_ID_COUNT;
+	int		l_iFori = 0;
+	*e_piNumMatched = 0;
+	if (e_pStreamingDataReceiver->iSize < MESSAGE_ID_INDEX)
+		return 0;
+	for (; l_iFori < l_iBufferSize; ++l_iFori)
+	{
+		if (e_pStreamingDataReceiver->pBuffer[l_iFori    ] == g_i8StreamingDataMagicID[0] &&
+			e_pStreamingDataReceiver->pBuffer[l_iFori + 1] == g_i8StreamingDataMagicID[1] &&
+			e_pStreamingDataReceiver->pBuffer[l_iFori + 2] == g_i8StreamingDataMagicID[2] &&
+			e_pStreamingDataReceiver->pBuffer[l_iFori + 3] == g_i8StreamingDataMagicID[3])
+		{
+			if (*e_piNumMatched >= NUM_MAGIC_ID_MATCHED)
+				return *e_piNumMatched;
+			e_piMatchedIndex[*e_piNumMatched] = l_iFori;
+			++*e_piNumMatched;
+		}
+	}
+	return *e_piNumMatched;
+}
+
+inline char*	BufferAndSizeGetData(sBufferAndSize*e_pStreamingDataReceiver, int*e_piSize,int16*e_pi16MessageID)
+{
+	char*	l_pOutData = 0;
+	int		l_iMatchIndex[NUM_MAGIC_ID_MATCHED];
+	int		l_iCheckSumIndex = -1;
+	int		l_iPacketSizeIndex = -1;
+	uint16	l_iu16PacketSize = -1;
+	int		l_iFori = 0;
+	int		l_iNumMatch = 0;
+	int		l_iBufferEndIndex = -1;
+	uint16	l_ui16CheckSum = -1;
+	uint16*	l_pui16OriginalCheckSumValue = 0;
+	int		l_iMessageIDIndex = MESSAGE_ID_INDEX;
+	if(!BufferAndSizeGetMatchedMagicID(e_pStreamingDataReceiver,l_iMatchIndex,&l_iNumMatch))
+		return 0;
+	for (; l_iFori < l_iNumMatch; ++l_iFori)
+	{
+		l_iPacketSizeIndex = l_iMatchIndex[l_iFori] + PACKET_SIZE_INDEX;
+		l_iBufferEndIndex = l_iMatchIndex[l_iFori] +*(uint16*)&e_pStreamingDataReceiver->pBuffer[l_iPacketSizeIndex];
+		if (l_iBufferEndIndex > e_pStreamingDataReceiver->iSize)
+		{
+			continue;
+		}
+		l_iCheckSumIndex = l_iMatchIndex[l_iFori] + CHECK_SUM_INDEX;
+		l_iu16PacketSize = *(uint16*)&e_pStreamingDataReceiver->pBuffer[l_iPacketSizeIndex];
+		l_ui16CheckSum = *(uint16*)&e_pStreamingDataReceiver->pBuffer[l_iCheckSumIndex];
+		l_pui16OriginalCheckSumValue = (uint16*)&e_pStreamingDataReceiver->pBuffer[l_iCheckSumIndex];
+		*l_pui16OriginalCheckSumValue = 0;
+		if (l_ui16CheckSum != BufferAndSizeGetCheckSum(&e_pStreamingDataReceiver->pBuffer[l_iMatchIndex[l_iFori]], l_iu16PacketSize))
+		{
+			printf("check sum not matched!");
+			continue;
+		}
+		l_pOutData = (char*)malloc(l_iu16PacketSize);
+		memcpy(l_pOutData, &e_pStreamingDataReceiver->pBuffer[l_iMatchIndex[l_iFori]], l_iu16PacketSize);
+		BufferAndSizeRemoveBuffer(e_pStreamingDataReceiver, l_iBufferEndIndex);
+		*e_pi16MessageID = *(int16*)&l_pOutData[l_iMessageIDIndex];
+		return l_pOutData;
+	}
+	return 0;
+}
