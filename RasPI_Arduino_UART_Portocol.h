9b73e8246ba5afd12a194c6e9345437df4be9555
HC050 <design3@hong-chiang.com.tw>
2019-08-21 15:25:38 +0800

    test2

--- RasPI_Arduino_UART_Portocol.h
+++ RasPI_Arduino_UART_Portocol.h
@@ -16,15 +16,15 @@ enum eArduinoRasPIMessage
 #pragma pack(push)  /* push current alignment to stack */
 #pragma pack(1)     /* set alignment to n byte boundary */
 LAZY_HEADER_STAR(eARM_A2R_HWARDWARE_INFO)
-	char		iExpcptionCode;  //   異常代碼 0=正常  1=馬達異常 2=避障感測器觸發  
-	uint32	ui16TagID;      //目前最新讀到的TAG ID  範圍0000000~FFFFFFF  
+	char		i8ExpcptionCode;  //   異常代碼 0=正常  1=馬達異常 2=避障感測器觸發  
+	uint32	ui32TagID;      //目前最新讀到的TAG ID  範圍0000000~FFFFFFF  
 //	char	i8Obstacle;   //合併到異常代碼中  
 	char	i8MealSettle;   //餐盤感測器狀態   00=沒有   01=第一節車有 10=第二節車有  11=兩節車都有     
 	char	i8Power;        //電池電量   0~100(%)
-	char	i16LeftSpeed;//讀取左馬達速度 0~100(%)    
-	char	i16RightSpeed;//讀取右馬達速度 0~100(%)  
-	uint32	ui16LeftDistabce;   //讀取左馬達轉動距離 範圍0~2147483648(單位mm) 
-	uint32	ui16RightDistabce;  //讀取右馬達轉動距離 範圍0~2147483648(單位mm) 
+	char	i816LeftSpeed;//讀取左馬達速度 0~100(%)    
+	char	i816RightSpeed;//讀取右馬達速度 0~100(%)  
+	uint32	ui32LeftDistabce;   //讀取左馬達轉動距離 範圍0~2147483648(單位mm) 
+	uint32	ui32RightDistabce;  //讀取右馬達轉動距離 範圍0~2147483648(單位mm) 
 	char	i8LeftMotorLoading;   //讀取左馬達負載量 範圍0~100(%) 
 	char	i8RightMotorLoading;  //讀取右馬達負載量 範圍0~100(%) 
 LAZY_HEADER_END(eARM_A2R_HWARDWARE_INFO)
@@ -34,14 +34,14 @@ LAZY_HEADER_END(eARM_A2R_HWARDWARE_INFO)
 #pragma pack(push)  /* push current alignment to stack */
 #pragma pack(1)     /* set alignment to n byte boundary */
 LAZY_HEADER_STAR(eARM_R2A_COMMAND)
-	uint32 iDistanceLeft;  //設定左馬達轉動距離 範圍0~2147483648(單位mm) 
-	uint32 iDistanceRight; //設定右馬達轉動距離 範圍0~2147483648(單位mm)
-  char iAcceLeft;     //設定左馬達加速度 範圍0~200(%)
-	char iAcceRight;    //設定右馬達加速度 範圍0~200(%)
-	char iSpeedLeft;    //設定左馬達速度  範圍0~100(%)
-	char iSpeedRight;   //設定右馬達速度  範圍0~100(%)
-	char ivoice;        //設定語音模組  0=停止 1=撥放  
-	uint32 i16TagID;   //設定目標TAG ID  範圍0000000~FFFFFFF  
+	uint32 ui32DistanceLeft;  //設定左馬達轉動距離 範圍0~2147483648(單位mm) 
+	uint32 ui32DistanceRight; //設定右馬達轉動距離 範圍0~2147483648(單位mm)
+  char i8AcceLeft;     //設定左馬達加速度 範圍0~200(%)
+	char i8AcceRight;    //設定右馬達加速度 範圍0~200(%)
+	char i8SpeedLeft;    //設定左馬達速度  範圍0~100(%)
+	char i8SpeedRight;   //設定右馬達速度  範圍0~100(%)
+	char i8voice;        //設定語音模組  0=停止 1=撥放  
+	uint32 i32TagID;   //設定目標TAG ID  範圍0000000~FFFFFFF  
 LAZY_HEADER_END(eARM_R2A_COMMAND)
 #pragma pack()     /* cancel previous alignment to n byte boundary */ 
 
