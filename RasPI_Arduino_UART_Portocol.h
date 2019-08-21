03cd17e30b7f9d5e17bc8beb8a7157d04d5f26e4
HC050 <design3@hong-chiang.com.tw>
2019-08-21 15:16:30 +0800

    test

--- RasPI_Arduino_UART_Portocol.h
+++ RasPI_Arduino_UART_Portocol.h
@@ -16,17 +16,17 @@ enum eArduinoRasPIMessage
 #pragma pack(push)  /* push current alignment to stack */
 #pragma pack(1)     /* set alignment to n byte boundary */
 LAZY_HEADER_STAR(eARM_A2R_HWARDWARE_INFO)
-	int		iExpcptionCode;//
-	uint16	ui16TagID;
-	char	i8Obstacle;
-	char	i8MealSettle;
-	char	i8Power;
-	int16	i16LeftSpeed;//-100,100
-	int16	i16RightSpeed;//-100,100
-	uint16	ui16LeftDistabce;
-	uint16	ui16RightDistabce;
-	char	i8LeftMotorLoading;
-	char	i8RightMotorLoading;
+	char		iExpcptionCode;  //   異常代碼 0=正常  1=馬達異常 2=避障感測器觸發  
+	uint32	ui16TagID;      //目前最新讀到的TAG ID  範圍0000000~FFFFFFF  
+//	char	i8Obstacle;   //合併到異常代碼中  
+	char	i8MealSettle;   //餐盤感測器狀態   00=沒有   01=第一節車有 10=第二節車有  11=兩節車都有     
+	char	i8Power;        //電池電量   0~100(%)
+	char	i16LeftSpeed;//讀取左馬達速度 0~100(%)    
+	char	i16RightSpeed;//讀取右馬達速度 0~100(%)  
+	uint32	ui16LeftDistabce;   //讀取左馬達轉動距離 範圍0~2147483648(單位mm) 
+	uint32	ui16RightDistabce;  //讀取右馬達轉動距離 範圍0~2147483648(單位mm) 
+	char	i8LeftMotorLoading;   //讀取左馬達負載量 範圍0~100(%) 
+	char	i8RightMotorLoading;  //讀取右馬達負載量 範圍0~100(%) 
 LAZY_HEADER_END(eARM_A2R_HWARDWARE_INFO)
 #pragma pack()     /* cancel previous alignment to n byte boundary */ 
 
@@ -34,12 +34,14 @@ LAZY_HEADER_END(eARM_A2R_HWARDWARE_INFO)
 #pragma pack(push)  /* push current alignment to stack */
 #pragma pack(1)     /* set alignment to n byte boundary */
 LAZY_HEADER_STAR(eARM_R2A_COMMAND)
-	int iDistance;
-	int iAcceLeft;
-	int iAcceRight;
-	int iSpeedLeft;
-	int iSpeedRight;
-	int16 i16TagID;
+	uint32 iDistanceLeft;  //設定左馬達轉動距離 範圍0~2147483648(單位mm) 
+	uint32 iDistanceRight; //設定右馬達轉動距離 範圍0~2147483648(單位mm)
+  char iAcceLeft;     //設定左馬達加速度 範圍0~200(%)
+	char iAcceRight;    //設定右馬達加速度 範圍0~200(%)
+	char iSpeedLeft;    //設定左馬達速度  範圍0~100(%)
+	char iSpeedRight;   //設定右馬達速度  範圍0~100(%)
+	char ivoice;        //設定語音模組  0=停止 1=撥放  
+	uint32 i16TagID;   //設定目標TAG ID  範圍0000000~FFFFFFF  
 LAZY_HEADER_END(eARM_R2A_COMMAND)
 #pragma pack()     /* cancel previous alignment to n byte boundary */ 
 
@@ -50,7 +52,7 @@ LAZY_HEADER_END(eARM_R2A_COMMAND)
 //int	SendDataTestCode(char*e_pBuffer, int e_iSizeToReceived)
 //{
 //	sStreamingData_eARM_A2R_HWARDWARE_INFO l_Data;
-//	l_Data.i16Message;
+//	l_Data.i16Message;      
 //	l_Data.i16PacketSize;
 //	//l_Data.ui8MagicID;
 //	l_Data.ui16LeftSpeed = 1;
