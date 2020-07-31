53a7d4acc29d55a917f730eb652d59a90c1f8eeb
Fatming <osimejp@yahoo.co.jp>
2020-07-31 19:16:42 +0800

    add UDP Camera rest code

--- CarGoUDPSharedProtocol.h
+++ CarGoUDPSharedProtocol.h
@@ -1,13 +1,20 @@
 #pragma once
 
 #include "SharedProtocolCommonDefine.h"
-
+#define	UDP_PORT	22667
 enum eUDP_MessageList
 {
 	eUDPML_C2S_REQUIRE_IMAGE = 200000,
 	eUDPML_S2C_IMAGE_PACKET_START,
 	eUDPML_S2C_IMAGE_PACKET,
 	eUDPML_MAX
+};
+
+enum eCompressMethod
+{
+	eCM_JPG = 0,
+	eCM_PNG,
+	eCM_MAX,
 };
 
 LAZY_MESSAGE_HEADER_STAR(eUDPML_C2S_REQUIRE_IMAGE)
@@ -21,6 +28,7 @@ LAZY_MESSAGE_HEADER_STAR(eUDPML_S2C_IMAGE_PACKET_START)
 	int32	i32EachPacketDataSize;
 	int		iImageWidth;
 	int		iImageHeight;
+	int		iNumChannel;
 LAZY_MESSAGE_HEADER_END(eUDPML_S2C_IMAGE_PACKET_START)
 
 
