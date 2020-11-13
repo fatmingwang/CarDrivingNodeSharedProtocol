2f8d41d49a215345adb926258cccd1cd01e3b7ed
Fatming <osimejp@yahoo.co.jp>
2020-11-13 17:50:23 +0800

    add some code for File Transfer

--- FileTransfer.cpp
+++ FileTransfer.cpp
@@ -1,10 +1,23 @@
 #include "stdafx.h"
 #include "FileTransfer.h"
 #include "CarGoAppSharedProtocol.h"
-
-cFileReceiver::cFileReceiver()
+#define	RECEIVE_PACKET_TIME_OUT	3.f
+#ifdef WIN32
+#pragma warning( disable : 4996 )
+#endif
+cFileReceiver::cFileReceiver(cGameNetwork* e_pGameNetwork, int e_iID)
 {
+	m_iID = e_iID;
+	m_pGameNetwork = e_pGameNetwork;
 	m_pFile = nullptr;
+	m_iFileSize = -1;
+	m_iReceivedFileSize = -1;
+	m_iNumPacket = -1;
+	m_iCurrentPacketIndex = -1;
+	m_bReceiveFileStart = false;
+	m_TimeOutTC.SetTargetTime(RECEIVE_PACKET_TIME_OUT);
+	REG_NET_MESSAGE_FUNCTION(eFT_C2S_FILE_TRANSFER_START_REQUIRED, &cFileReceiver::Process_eFT_C2S_FILE_TRANSFER_START_REQUIRED);
+	REG_NET_MESSAGE_FUNCTION(eFT_C2S_FILE_TRANSFER_DATA_REQUIRED, &cFileReceiver::Process_eFT_C2S_FILE_TRANSFER_DATA_REQUIRED);
 }
 
 cFileReceiver::~cFileReceiver()
@@ -14,7 +27,7 @@ cFileReceiver::~cFileReceiver()
 }
 
 
-bool cFileReceiver::Process_FILE_TRANSFER_START_REQUIRED(sNetworkReceivedPacket* e_pData)
+bool cFileReceiver::Process_eFT_C2S_FILE_TRANSFER_START_REQUIRED(sNetworkReceivedPacket* e_pData)
 {
 	CAR_GO_APP_DO_RETURN_FALSE_IF_SIZE_INCORRECT(sNetwork_eFT_C2S_FILE_TRANSFER_START_REQUIRED, e_pData->iSize);
 	sNetwork_eFT_C2S_FILE_TRANSFER_START_REQUIRED*l_pData = (sNetwork_eFT_C2S_FILE_TRANSFER_START_REQUIRED*)e_pData->pData;
@@ -23,37 +36,212 @@ bool cFileReceiver::Process_FILE_TRANSFER_START_REQUIRED(sNetworkReceivedPacket*
 	m_iNumPacket = l_pData->iNumPacket;
 	m_iCurrentPacketIndex = 0;
 	m_iReceivedFileSize = 0;
-	m_TC.Update();
-	return false;
+	if (m_pFile)
+	{
+		NvFClose(m_pFile);
+		m_pFile = nullptr;
+	}
+	m_pFile = MyFileOpen(m_strFileFullName.c_str(),"w");
+	if (!m_pFile)
+	{
+		m_bReceiveFileStart = false;
+		sNetwork_eFT_C2S_FILE_TRANSFER_START_RESULT l_Data;
+		l_Data.iResultCode = 0;
+		l_Data.iID = m_iID;;
+		m_pGameNetwork->SendDataToServer<sNetwork_eFT_C2S_FILE_TRANSFER_START_RESULT>(&l_Data,false);
+	}
+	else
+	{
+		m_bReceiveFileStart = true;
+	}
+	return true;
 }
 
-bool cFileReceiver::Process_FILE_TRANSFER_DATA_REQUIRED(sNetworkReceivedPacket* e_pData)
+bool cFileReceiver::Process_eFT_C2S_FILE_TRANSFER_DATA_REQUIRED(sNetworkReceivedPacket* e_pData)
 {
+	if (!m_bReceiveFileStart)
+		return false;
 	CAR_GO_APP_DO_RETURN_FALSE_IF_SIZE_INCORRECT(sNetwork_eFT_C2S_FILE_TRANSFER_DATA_REQUIRED, e_pData->iSize);
 	sNetwork_eFT_C2S_FILE_TRANSFER_DATA_REQUIRED*l_pData = (sNetwork_eFT_C2S_FILE_TRANSFER_DATA_REQUIRED*)e_pData->pData;
-	l_pData->iPacketIndex;
-	l_pData->iDataLen;
-	l_pData->iSize;
-	l_pData->cData;
+	bool l_bFailed = false;
+	if (this->m_iCurrentPacketIndex != l_pData->iPacketIndex)
+	{
+		l_bFailed = true;
+	}
+	if (m_pFile)
+	{
+		auto l_uiSize = NvFWrite(l_pData->cData, 1, l_pData->iDataLen, m_pFile);
+		if (l_pData->iDataLen != l_uiSize)
+		{
+			l_bFailed = true;
+		}
+		else
+		{
+			m_iReceivedFileSize += l_pData->iDataLen;
+		}
+	}
+	if (l_bFailed)
+	{
+		m_bReceiveFileStart = false;
+		sNetwork_eFT_C2S_FILE_TRANSFER_DATA_RESULT l_Data;
+		l_Data.iResultCode = 0;
+		l_Data.iID = m_iID;
+		m_pGameNetwork->SendDataToServer<sNetwork_eFT_C2S_FILE_TRANSFER_DATA_RESULT>(&l_Data, false);
+		NvFClose(m_pFile);
+		m_pFile = nullptr;
+	}
+	else
+	{
+		this->m_bReceiveFileStart = false;
+		m_TimeOutTC.Start();
+		++m_iCurrentPacketIndex;
+		if (m_iCurrentPacketIndex == m_iNumPacket &&
+			m_iReceivedFileSize == m_iFileSize)
+		{//finish
+			NvFClose(m_pFile);
+			m_pFile = nullptr;
+		}
+		
+	}
 	return false;
 }
 
 void cFileReceiver::Update(float e_fElpaseTime)
 {
+	if (this->m_bReceiveFileStart)
+	{
+		m_TimeOutTC.Update(e_fElpaseTime);
+	}
 }
 
 int cFileReceiver::GetProgress()
 {
 	if (m_iFileSize != 0)
 	{
-		return ((float)m_iReceivedFileSize / m_iFileSize)*100;
+		return (int)(((float)m_iReceivedFileSize / m_iFileSize)*100);
 	}
 	return 0;
 }
 
 bool cFileReceiver::IsTimeout()
 {
-	if (m_TC.fElpaseTime > 1.f)
+	if (m_TimeOutTC.bTragetTimrReached)
 		return true;
 	return false;
+}
+
+cFileSender::cFileSender()
+{
+	m_pFile = nullptr;
+	m_fProgress = 0.f;
+	m_TimeOutTC.SetTargetTime(RECEIVE_PACKET_TIME_OUT);
+	m_iRestDataToSendFileSize = 0;
+	m_bSendToClient = false;
+}
+
+cFileSender::~cFileSender()
+{
+}
+
+bool cFileSender::OpenFileCollectData(const char* e_strFileName, const char* e_strTragetMachineFullFileName)
+{
+	m_strTagetMachineFileFullName = e_strTragetMachineFullFileName;
+	m_fProgress = 0.f;
+	m_pFile = MyFileOpen(e_strFileName, "r");
+	if (m_pFile)
+	{
+		m_iFileSize = (int)GetFileSize(m_pFile);
+		m_iRestDataToSendFileSize = m_iFileSize;
+		m_iNumPacket = m_iFileSize / TRANSFER_FILE_PACK_SIZE;
+		if (m_iFileSize % TRANSFER_FILE_PACK_SIZE == 0)
+			m_iNumPacket += 1;
+		m_iCurrentPacketIndex = 0;
+		m_TimeOutTC.Start();
+		return true;
+	}
+	return false;
+}
+
+void cFileSender::SendFileThreadUpdate(float e_fElpaseTime)
+{
+	bool l_bSentOk = false;
+	if (m_iCurrentPacketIndex == 0)
+	{
+		sNetwork_eFT_C2S_FILE_TRANSFER_START_REQUIRED l_Data;
+		l_Data.iFileSize = m_iFileSize;
+		l_Data.iNumPacket = m_iNumPacket;
+		sprintf(l_Data.strFileFullName,"%s",m_strTagetMachineFileFullName.c_str());
+		this->m_pGameNetwork->SendDataToServer<sNetwork_eFT_C2S_FILE_TRANSFER_START_REQUIRED>(&l_Data);
+	}
+	else
+	{
+		sNetwork_eFT_C2S_FILE_TRANSFER_DATA_REQUIRED l_Data;
+		l_Data.iPacketIndex = m_iCurrentPacketIndex;
+		if(m_iRestDataToSendFileSize >= TRANSFER_FILE_PACK_SIZE)
+			l_Data.iDataLen = TRANSFER_FILE_PACK_SIZE;
+		else
+			l_Data.iDataLen = m_iRestDataToSendFileSize;
+		bool l_bFileCorrect = false;
+		if (NvFSeek(m_pFile, m_iCurrentPacketIndex * TRANSFER_FILE_PACK_SIZE, SEEK_SET) == 0)
+		{
+			if (NvFRead(l_Data.cData, 1, l_Data.iDataLen, m_pFile) == l_Data.iDataLen)
+			{
+				l_bSentOk = SendData<sNetwork_eFT_C2S_FILE_TRANSFER_DATA_REQUIRED>(&l_Data,false, m_bSendToClient);
+				if (l_bSentOk)
+				{
+					m_iRestDataToSendFileSize -= l_Data.iDataLen;
+					if (m_iRestDataToSendFileSize == 0)
+					{
+						FMLog::Log("send file finish",true);
+					}
+				}
+				l_bFileCorrect = true;
+			}			
+		}
+		if (!l_bFileCorrect)
+		{
+			l_Data.iDataLen = -1;
+		}
+	}
+	++m_iCurrentPacketIndex;
+	if (m_iCurrentPacketIndex >= m_iNumPacket)
+	{
+		NvFClose(this->m_pFile);
+		m_pFile = nullptr;
+		this->m_bLeaveThread = true;
+	}
+	if (!l_bSentOk)
+	{
+		m_TimeOutTC.Update(e_fElpaseTime);
+	}
+	else
+	{
+		m_TimeOutTC.Start();
+	}
+}
+
+
+bool cFileSender::SendFile(const char* e_strFileName, const char* e_strTragetMachineFullFileName, cGameNetwork* e_pGameNetwork, bool e_bToClient)
+{
+	if (IsThreadWorking())
+		return false;
+	if (OpenFileCollectData(e_strFileName, e_strTragetMachineFullFileName))
+	{
+		m_bSendToClient = e_bToClient;
+		this->ThreadDetach(std::bind(&cFileSender::SendFileThreadUpdate, this, std::placeholders::_1), "cFileSender::SendFileThreadUpdate");
+		return true;
+	}
+	return false;
+}
+
+int cFileSender::GetProgress()
+{
+	if (m_iNumPacket == 0)
+		return -1;
+	return (int)(((float)m_iCurrentPacketIndex / m_iNumPacket)*100);
+}
+
+bool cFileSender::IsTimeout()
+{
+	return m_TimeOutTC.bTragetTimrReached;
 }
--- FileTransfer.h
+++ FileTransfer.h
@@ -4,12 +4,14 @@
 #endif
 
 #include "SharedProtocolCommonDefine.h"
+#include <functional>
 enum eFileTransfer
 {
 	//reserved those message for file transfer
-	eFT_C2S_FILE_TRANSFER_START_REQUIRED = 56745,
-	eFT_C2S_FILE_TRANSFER_DATA_REQUIRED = 56746,
-	eFT_C2S_FILE_TRANSFER_DATA_RESULT = 56747,//if data file size not match return 1,if packet not sequence 2.
+	eFT_C2S_FILE_TRANSFER_START_REQUIRED = 56745,	//start to send file
+	eFT_C2S_FILE_TRANSFER_START_RESULT,				//create file result  for ok else for failed
+	eFT_C2S_FILE_TRANSFER_DATA_REQUIRED,			//send packet.
+	eFT_C2S_FILE_TRANSFER_DATA_RESULT				//if data file size not match return 1,if packet not sequence 2.0 for ok.
 };
 
 //
@@ -19,6 +21,12 @@ LAZY_MESSAGE_HEADER_STAR(eFT_C2S_FILE_TRANSFER_START_REQUIRED)
 	int		iNumPacket;
 LAZY_MESSAGE_HEADER_END(eFT_C2S_FILE_TRANSFER_START_REQUIRED)
 
+LAZY_RESULT_MESSAGE_HEADER_STAR(eFT_C2S_FILE_TRANSFER_START_RESULT)
+	int iID;
+//iResultCode 0 for create file failed 1 for success
+LAZY_RESULT_MESSAGE_HEADER_END(eFT_C2S_FILE_TRANSFER_START_RESULT)
+
+
 #define	TRANSFER_FILE_PACK_SIZE	4096
 LAZY_MESSAGE_HEADER_STAR(eFT_C2S_FILE_TRANSFER_DATA_REQUIRED)
 	int		iPacketIndex;
@@ -26,7 +34,14 @@ LAZY_MESSAGE_HEADER_STAR(eFT_C2S_FILE_TRANSFER_DATA_REQUIRED)
 	int		iDataLen;
 LAZY_MESSAGE_HEADER_END(eFT_C2S_FILE_TRANSFER_DATA_REQUIRED)
 
-class cFileReceiver:public NamedTypedObject
+
+LAZY_RESULT_MESSAGE_HEADER_STAR(eFT_C2S_FILE_TRANSFER_DATA_RESULT)
+//iResultCode 0 for ok,1 for packet index not correct,2 for size not match
+	int iID;
+LAZY_RESULT_MESSAGE_HEADER_END(eFT_C2S_FILE_TRANSFER_DATA_RESULT)
+
+
+class cFileReceiver:public NamedTypedObject, public cMessageSender
 {
 	NvFile*				m_pFile;
 	std::string			m_strFileFullName;
@@ -34,25 +49,56 @@ class cFileReceiver:public NamedTypedObject
 	int					m_iReceivedFileSize;
 	int					m_iNumPacket;
 	int					m_iCurrentPacketIndex;
-	UT::sTimeAndFPS		m_TC;
-	bool				Process_FILE_TRANSFER_START_REQUIRED(sNetworkReceivedPacket* e_pData);
-	bool				Process_FILE_TRANSFER_DATA_REQUIRED(sNetworkReceivedPacket* e_pData);
+	UT::sTimeCounter	m_TimeOutTC;
+	bool				m_bReceiveFileStart;
+	//
+	cGameNetwork*		m_pGameNetwork;
+	int					m_iID;//
+	//
+	bool				Process_eFT_C2S_FILE_TRANSFER_START_REQUIRED(sNetworkReceivedPacket* e_pData);
+	bool				Process_eFT_C2S_FILE_TRANSFER_DATA_REQUIRED(sNetworkReceivedPacket* e_pData);
+	//
 public:
-	cFileReceiver();
+	cFileReceiver(cGameNetwork* e_pGameNetwork,int e_iID);
 	virtual ~cFileReceiver();
 	void		Update(float e_fElpaseTime);
 	int			GetProgress();
 	bool		IsTimeout();
+	bool		IsReceiveFileStart() { return m_bReceiveFileStart; }
 };
 
-
-class cFileSender :public NamedTypedObject
+class cFileSender :public NamedTypedObject,public cCPP11Thread,public cMessageSender
 {
-	void		SendFileUpdate(float e_fElpaseTime);
+	template<class TYPE>bool		SendData(TYPE* e_Data, bool e_bDoMultothread, bool e_bSendToClient);
+
+	bool				OpenFileCollectData(const char* e_strFileName, const char* e_strTragetMachineFullFileName);
+	void				SendFileThreadUpdate(float e_fElpaseTime);
+	NvFile*				m_pFile;
+	std::string			m_strTagetMachineFileFullName;
+	float				m_fProgress;
+	int					m_iFileSize;
+	int					m_iNumPacket;
+	int					m_iCurrentPacketIndex;
+	int					m_iRestDataToSendFileSize;
+	bool				m_bSendToClient;
+	//for internet.
+	UT::sTimeCounter	m_TimeOutTC;
+	cGameNetwork*		m_pGameNetwork;
+	bool				Process_eFT_C2S_FILE_TRANSFER_START_RESULT(sNetworkReceivedPacket* e_pData);
+	bool				Process_eFT_C2S_FILE_TRANSFER_DATA_RESULT(sNetworkReceivedPacket* e_pData);
 public:
 	cFileSender();
 	virtual ~cFileSender();
-	void		SendFile(const char* e_strFileName,const char*e_strTragetMachineFullFileName);
+	bool		SendFile(const char* e_strFileName,const char*e_strTragetMachineFullFileName,cGameNetwork*e_pGameNetwork,bool e_bToClient);
 	int			GetProgress();
 	bool		IsTimeout();
 };
+
+template<class TYPE>bool				cFileSender::SendData(TYPE* e_Data, bool e_bDoMultothread, bool e_bSendToClient)
+{
+	if (e_bSendToClient)
+	{
+		return m_pGameNetwork->SendDataToAllClient<TYPE>(e_Data, e_bDoMultothread);
+	}
+	return m_pGameNetwork->SendDataToServer<TYPE>(e_Data, e_bDoMultothread);
+}
